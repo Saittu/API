fdd930a45da26aa5397a938223e5461fb70518cf
Jean <jeanants@icloud.com>
2023-12-24 00:49:14 -0300

    First commit

---
+++ src/database/knex/migrations/20231223200531_creatTags.js
@@ -0,0 +1,15 @@
+
+exports.up = knex => knex.schema.createTable("tags", table => {
+    table.increments("id")
+
+    table.integer("notes_id").references("id").inTable("notes").onDelete("CASCADE")
+    table.integer("user_id").references("id").inTable("users")
+
+    table.text("name").notNullable()
+
+}) 
+  
+
+
+
+exports.down = knex => knex.schema.dropTable("tags")
---
+++ src/routes/users.routes.js
@@ -0,0 +1,24 @@
+const { Router } = require ("express")
+
+const UserController = require("../controllers/UserController")
+
+const usersRoutes = Router()
+
+
+// function myMiddleware (request, response, next){
+//     console.log("Você passou pelo Middleware!")
+//     if (!request.body.isAdmin){
+//         return response.json({message: "user unauthorized"})
+//     }
+
+//     next()
+// }
+
+
+
+const userController = new UserController()
+
+usersRoutes.post("/",  userController.create)
+usersRoutes.put("/:id", userController.update)
+
+module.exports = usersRoutes
